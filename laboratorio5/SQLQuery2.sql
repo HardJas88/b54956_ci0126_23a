4845f0252feebd1c1e6caafe34a15b379c6e05b9
HardJas88 <https://github.com/HardJas88/b54956_ci0126_23a.git>
2023-05-01 23:39:09 -0600

    Se agrega Laboratorio5

---
+++ laboratorio5/SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[Pelicula] ([Nombre],[Año])
+VALUES('Guardians of the Galaxy Vol 3', '2023'),
+('Spider-Man: No Way Home', '2022')
+GO
---
+++ laboratorio5/laboratorio5/Models/PeliculaModelo.cs
@@ -0,0 +1,9 @@
+﻿namespace laboratorio5.Models
+{
+    public class PeliculaModelo
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Año{ get; set; }
+    }
+}
